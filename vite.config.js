e88508bc9e4141bff0a596a0237f2da485f09e24
lkhee <slaqmtm0735@gmail.com>
2025-06-16 15:54:38 +0900

    chore: 환경변수 수정

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
+import process from 'node:process'
 import vue from "@vitejs/plugin-vue"
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
-  },
+export default defineConfig(({ mode }) => {
+  const env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  return {
+    define: {
+      VITE_SERVICE_TITLE: JSON.stringify(env.VITE_APP_TITLE),
+      VITE_DEFAULT_API_URL: JSON.stringify(env.VITE_APP_DEFAULT_API_URL),
+      VITE_MASTER_API_URL: JSON.stringify(env.VITE_APP_MASTER_API_URL),
+      VITE_USER_API_URL: JSON.stringify(env.VITE_APP_USER_API_URL),
+      'process.env': {},
+      'window.global': {},
+    },
+    plugins: [vue()],
+    resolve: {
+      alias: [{ find: "@", replacement: "/src" }],
+    },
+  }
 })
